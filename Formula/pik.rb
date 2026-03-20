d2a676ab2f8dec6b4537cbfde07bb9015e15d7c0
Jacek Kurlit <jacek.kurlit@gmail.com>
2026-03-21 00:38:55 +0100

    fix pik buil

--- Formula/pik.rb
+++ Formula/pik.rb
@@ -5,22 +5,22 @@ class Pik < Formula
 
     on_macos do
       on_arm do
-        url "https://github.com/jacek-kurlit/pik/releases/download/#{version}/pik-#{version}-aarch64-apple-darwin.tar.gz"
+        url "https://github.com/jacek-kurlit/pik/releases/download/0.30.1/pik-0.30.1-aarch64-apple-darwin.tar.gz"
         sha256 "a0f16f5ccf4d2f162f05b532d0b1791ceb78346902bb95a4539934c7a2e2fdbd"
       end
       on_intel do
-        url "https://github.com/jacek-kurlit/pik/releases/download/#{version}/pik-#{version}-x86_64-apple-darwin.tar.gz"
+        url "https://github.com/jacek-kurlit/pik/releases/download/0.30.1/pik-0.30.1-x86_64-apple-darwin.tar.gz"
         sha256 "1457026726cd02abdebfebf3dc88fc8ed926a1933d28523ffdf99203f73d9ca5"
       end
     end
 
     on_linux do
       on_arm do
-        url "https://github.com/jacek-kurlit/pik/releases/download/#{version}/pik-#{version}-aarch64-unknown-linux-gnu.tar.gz"
+        url "https://github.com/jacek-kurlit/pik/releases/download/0.30.1/pik-0.30.1-aarch64-unknown-linux-gnu.tar.gz"
         sha256 "ca5686e844b59222c79a084e7e91d1236c92b16e78e6b89c611690cb3edf65a7"
       end
       on_intel do
-        url "https://github.com/jacek-kurlit/pik/releases/download/#{version}/pik-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+        url "https://github.com/jacek-kurlit/pik/releases/download/0.30.1/pik-0.30.1-x86_64-unknown-linux-gnu.tar.gz"
         sha256 "21d1873226aed620a485d762b5e56d10c8d772f70b799fabebf44440e86087d2"
       end
     end
